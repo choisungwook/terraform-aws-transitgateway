5fdf89bf75cf0ea6787dd67aa9e4e82dde33e175
choisungwook <kgg1959@naver.com>
2022-05-05 16:59:50 +0900

    vpc 추가

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-module "vpc" {
+module "vpc-1" {
   source = "./vpc"
 
-  name = "terraform-demo-vpc"
+  name = "terraform-demo-vpc1"
   cidr = "10.100.0.0/16"
   create_igw = true
 
@@ -9,6 +9,21 @@ module "vpc" {
     sub-1 = {
       az   = "ap-northeast-2c"
       cidr = "10.100.10.0/24"
+    }
+  }
+}
+
+module "vpc-2" {
+  source = "./vpc"
+
+  name = "terraform-demo-vpc2"
+  cidr = "10.200.0.0/16"
+  create_igw = true
+
+  public_subnets = {
+    sub-1 = {
+      az   = "ap-northeast-2c"
+      cidr = "10.200.10.0/24"
     }
   }
 }
