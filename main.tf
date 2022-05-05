689ca1077f644ba77016d7653ef20cca3caef219
choisungwook <kgg1959@naver.com>
2022-05-05 16:57:15 +0900

    cidr 수정

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ module "vpc" {
   source = "./vpc"
 
   name = "terraform-demo-vpc"
-  cidr = "192.168.0.0/16"
+  cidr = "10.100.0.0/16"
   create_igw = true
 
   public_subnets = {
     sub-1 = {
       az   = "ap-northeast-2c"
-      cidr = "192.168.1.0/24"
+      cidr = "10.100.10.0/24"
     }
   }
 }
